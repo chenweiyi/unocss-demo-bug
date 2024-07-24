64c8bb2dac9321002ef0649755ce21f960c3c9d6
chenweiyi <737649321@qq.com>
2024-07-24 12:52:30 +0800

    feat: add

--- src/jsx.tsx
+++ src/jsx.tsx
@@ -1,21 +1,6 @@
 const App = () => {
   return (
-  <div
-    className='
-    absolute 
-    w-60px 
-    h-200px 
-    left-30px 
-    absolute-y-center 
-    rounded-30px 
-    box-shadow-sample
-    flex
-    flex-col
-    justify-center
-    items-center
-    py-20px
-  '
-  ></div>)
+  <div className='py-20px'></div>)
 }
 
 export default App
